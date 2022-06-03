3b48794735d225593a784b6b8941a41b997bab71
Jaison Jacob <jacob.jaison@hotmail.com>
2022-06-03 18:50:47 +0400

    Changes - User model 03-Jun-22

--- config/index.js
+++ config/index.js
@@ -17,6 +17,12 @@ const favicon = require("serve-favicon");
 // https://www.npmjs.com/package/path
 const path = require("path");
 
+// Session requirement
+const session = require("express-session");
+
+// Mongo store
+const MongoStore = require("connect-mongo");
+
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
@@ -36,4 +42,20 @@ module.exports = (app) => {
 
   // Handles access to the favicon
   app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+
+  app.use(
+		session({
+			secret:
+				process.env.SESSION_SECRET ||
+				"A really random string, it doesn't really matter",
+			resave: false,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 60 * 60 * 24 * 7, // A week
+			},
+			store: MongoStore.create({
+				mongoUrl: process.env.MONGO_URI || "mongodb://localhost/book-store",
+			}),
+		})
+	);
 };
