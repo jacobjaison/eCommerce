18e09f80ab5c5300503d3faf87b121ddf8f5cc2d
Jaison Jacob <jacob.jaison@hotmail.com>
2022-06-09 23:29:37 +0400

    Updated Changes

--- middlewares/exposeUserToViews.js
+++ middlewares/exposeUserToViews.js
@@ -2,9 +2,13 @@ const app = require("../app")
 
 const exposeUsers = (req, res, next) => {
 	if (req.session.currentUser) {
+        if (req.session.currentUser.isAdmin) {
+            res.locals.isAdmin = true
+        }
 		res.locals.currentUser = req.session.currentUser;
        // app.locals.userTitle = req.session.currentUser;
 		res.locals.isLoggedIn = true;
+        res.locals.firstName = req.session.currentUser.firstName;
         
 	}
 
